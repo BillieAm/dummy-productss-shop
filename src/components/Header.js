de6344bc0cff19168a8afb7b3fbc5b94fbff7d1c
Billie Amit <biliamit@gmail.com>
2022-07-19 16:42:01 +0300

    set the basic structure

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import logo from "../logo.png";
+
+function Header() {
+  return (
+    <header>
+      <img src={logo} alt="dummy logo" />
+      <i class="ri-shopping-cart-line"></i>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <main>
+      <h2>Home page</h2>
+    </main>
+  );
+}
+
+export default Home;
